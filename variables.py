5d67a670b89e41c76fe46a0b1762f9defb21ebe6
Darshan Biswal <biswaldarshan7@gmail.com>
2026-06-21 17:18:00 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,23 @@
+a = 1 # intiger
+b = 2.34 # float
+c = "Darshan" # string
+d = False # boolean
+e = None # none 
+
+# a,b,c,d,e are variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+aaa= 7462
+
+Darshan7 = 484
+
+_Darshan = 875
+
+# A variable name can contain alphabets, digits, and underscores.
+# A variable name can only start with an alphabet and underscores.
+# A variable name canʼt start with a digit.
+# No white space is allowed to be used inside a variable name.
